25271b191b1bd52fe89a039bf4dc744de91e1268
Dieguinhophp <lorddedsbrgamers2@gmail.com>
2026-03-31 13:41:35 -0300

    implementação do servidor e dos clientes

---
+++ Cliente/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+class Cliente
+{
+    static TcpClient cliente;
+    static NetworkStream stream;
+
+    static void Main()
+    {
+        cliente = new TcpClient("127.0.0.1", 5000);
+        stream = cliente.GetStream();
+
+        Console.WriteLine("Conectado ao servidor!");
+
+        Thread t = new Thread(ReceberMensagens);
+        t.Start();
+
+        while (true)
+        {
+            string msg = Console.ReadLine();
+            byte[] dados = Encoding.UTF8.GetBytes(msg);
+            stream.Write(dados, 0, dados.Length);
+        }
+    }
+
+    static void ReceberMensagens()
+    {
+        byte[] buffer = new byte[1024];
+
+        while (true)
+        {
+            int bytes = stream.Read(buffer, 0, buffer.Length);
+            string mensagem = Encoding.UTF8.GetString(buffer, 0, bytes);
+
+            Console.WriteLine("Mensagem: " + mensagem);
+        }
+    }
+}
---
+++ Servidor/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+class Servidor
+{
+    static TcpListener servidor;
+    static List<TcpClient> clientes = new List<TcpClient>();
+
+    static void Main()
+    {
+        servidor = new TcpListener(IPAddress.Any, 5000);
+        servidor.Start();
+
+        Console.WriteLine("Servidor iniciado...");
+
+        while (true)
+        {
+            TcpClient cliente = servidor.AcceptTcpClient();
+            clientes.Add(cliente);
+
+            Console.WriteLine("Cliente conectado!");
+
+            Thread t = new Thread(() => AtenderCliente(cliente));
+            t.Start();
+        }
+    }
+
+    static void AtenderCliente(TcpClient cliente)
+    {
+        NetworkStream stream = cliente.GetStream();
+        byte[] buffer = new byte[1024];
+
+        while (true)
+        {
+            int bytes = stream.Read(buffer, 0, buffer.Length);
+            string mensagem = Encoding.UTF8.GetString(buffer, 0, bytes);
+
+            Console.WriteLine("Recebido: " + mensagem);
+
+            EnviarParaTodos(mensagem);
+        }
+    }
+
+    static void EnviarParaTodos(string mensagem)
+    {
+        byte[] dados = Encoding.UTF8.GetBytes(mensagem);
+
+        foreach (var cliente in clientes)
+        {
+            NetworkStream stream = cliente.GetStream();
+            stream.Write(dados, 0, dados.Length);
+        }
+    }
+}
